899bb020b65d7a7a80ac5cda65aef22b2cfe30ed
ballpenf <111833216+ballpenf@users.noreply.github.com>
2022-09-26 17:14:20 +0900

    0926 LKS

--- wms/src/main/java/com/noo/wms/company/service/CompanyServiceImpl.java
+++ wms/src/main/java/com/noo/wms/company/service/CompanyServiceImpl.java
@@ -9,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.noo.wms.company.mapper.CompanySQLMapper;
+import com.noo.wms.vo.AccountVo;
 import com.noo.wms.vo.AdminVo;
 import com.noo.wms.vo.CompanyVo;
+import com.noo.wms.vo.DepartmentVo;
+import com.noo.wms.vo.EmployeeRankVo;
 
 @Service
 public class CompanyServiceImpl {
@@ -56,6 +59,73 @@ public class CompanyServiceImpl {
 	
 		companyMapper.updateCompany(companyVo);
 		
+	}
+	
+	public ArrayList<DepartmentVo> departmentInfo(String searchType, String searchWord, int pageNum){
+
+		int startList = (pageNum-1)*15;
+		
+		ArrayList<DepartmentVo> accountList = companyMapper.departmentInfo(searchType, searchWord, startList);
+		
+		return accountList;
+	}
+	
+	public int departmentInfoCount(String searchType, String searchWord){
+		int departmentList = companyMapper.departmentInfoCount(searchType , searchWord);
+		
+		return departmentList;
+	}
+	
+	//부서 정보 등록
+	public void departmentRegister(DepartmentVo departmentVo) {
+
+		companyMapper.insertDepartment(departmentVo);
+	}
+	
+	public DepartmentVo selectDepartmentInfo(DepartmentVo departmentVo) {
+		
+		return companyMapper.selectDepartmentInfo(departmentVo);
+	}
+	
+	public void updateDepartmentInfo(DepartmentVo departmentVo) {
+		companyMapper.updateDepartmentInfo(departmentVo);
+	}
+	
+	public void deleteDepartmentInfo (DepartmentVo departmentVo) {
+		companyMapper.deleteDepartmentInfo(departmentVo);
+	}
+	
+	public ArrayList<EmployeeRankVo> employeeRankInfo(String searchType, String searchWord, int pageNum){
+
+		int startList = (pageNum-1)*15;
+		
+		ArrayList<EmployeeRankVo> employeeList = companyMapper.employeeRankInfo(searchType, searchWord, startList);
+		
+		return employeeList;
+	}
+	
+	public int employeeRankInfoCount(String searchType, String searchWord){
+		int employeeRankList = companyMapper.employeeRankInfoCount(searchType , searchWord);
+		
+		return employeeRankList;
+	}
+	
+	
+	public void insertEmployeeRank(EmployeeRankVo employeeRankVo) {
+		companyMapper.insertEmployeeRank(employeeRankVo);
+	}
+	
+	public EmployeeRankVo selectEmployeeRankInfo(EmployeeRankVo employeeRankVo) {
+		
+		return companyMapper.selectEmployeeRankInfo(employeeRankVo);
+	}
+	
+	public void updateEmployeeRankInfo(EmployeeRankVo employeeRankVo) {
+		companyMapper.updateEmployeeRankInfo(employeeRankVo);
+	}
+	
+	public void deleteEmployeeRankInfo (EmployeeRankVo employeeRankVo) {
+		companyMapper.deleteEmployeeRankInfo(employeeRankVo);
 	}
 	
 	
